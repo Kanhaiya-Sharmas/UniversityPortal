7c7e94fe3b474183851511ae689e0bb75f1accdb
Kanhaiya-Sharma <kanhaiya52cse@gmail.com>
2020-09-26 09:15:46 +0530

    adding model

--- details/models.py
+++ details/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Details(models.Model):
+    Mentor_Name = models.CharField(max_length=50,blank=False, default='')
+    Student_Name = models.CharField(max_length=50, blank=False, default='')
+    Status = models.CharField(max_length=50, blank=False, default='')
+    Remark = models.CharField(max_length=50, blank=False, default='')
