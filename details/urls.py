44e1ba72a49755b1a51e2a5d7194392107820eb5
Kanhaiya-Sharma <kanhaiya52cse@gmail.com>
2020-09-26 12:56:19 +0530

    added new function and url for mentor

--- details/urls.py
+++ details/urls.py
@@ -3,5 +3,8 @@ from details import views
 
 urlpatterns = [
     url(r'^api/mentor$', views.mentor_list),
+    url(r'^api/mentor/(?P<pk>[0-9]+)$', views.mentor_detail),
+    url(r'^api/student$', views.student_list),
+
 
 ]
--- details/views.py
+++ details/views.py
@@ -10,8 +10,10 @@ from rest_framework.decorators import api_view
 
 
 # Create your views here.
+# START function mentor_list
 @api_view(['GET', 'POST', 'DELETE'])
 def mentor_list(request):
+    # Get all mentor list
     if request.method == 'GET':
         details = Mentor.objects.all()
 
@@ -21,7 +23,7 @@ def mentor_list(request):
 
         details_serializer = MentorSerializer(details, many=True)
         return JsonResponse(details_serializer.data, safe=False)
-
+    # Add a new mentor
     elif request.method == 'POST':
         mentor_data = JSONParser().parse(request)
         mentor_serializer = MentorSerializer(data=mentor_data)
@@ -29,12 +31,39 @@ def mentor_list(request):
             mentor_serializer.save()
             return JsonResponse(mentor_serializer.data, status=status.HTTP_201_CREATED)
         return JsonResponse(mentor_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # Delete all mentor
+    elif request.method == 'DELETE':
+        count = Mentor.objects.all().delete()
+        return JsonResponse({'message': '{} details were deleted successfully!'.format(count[0])},
+                            status=status.HTTP_204_NO_CONTENT)
+    elif request.method == 'DELETE':
+        count = Mentor.objects.all().delete()
+        return JsonResponse({'message': '{} details were deleted successfully!'.format(count[0])},
+                            status=status.HTTP_204_NO_CONTENT)
+# END function mentor_lis
 
-    elif request.method == 'DELETE':
-        count = Mentor.objects.all().delete()
-        return JsonResponse({'message': '{} details were deleted successfully!'.format(count[0])},
-                            status=status.HTTP_204_NO_CONTENT)
-    elif request.method == 'DELETE':
-        count = Mentor.objects.all().delete()
-        return JsonResponse({'message': '{} details were deleted successfully!'.format(count[0])},
-                            status=status.HTTP_204_NO_CONTENT)
+
+# START function mentor_details by id
+@api_view(['GET', 'PUT', 'DELETE'])
+def mentor_detail(request, pk):
+    try:
+        mentor = Mentor.objects.get(pk=pk)
+
+        if request.method == 'GET':
+            detail_serializer = MentorSerializer(mentor)
+            return JsonResponse(detail_serializer.data)
+        elif request.method == 'PUT':
+            detail_data = JSONParser().parse(request)
+            detail_serializer = MentorSerializer(mentor, data=detail_data)
+            if detail_serializer.is_valid():
+                detail_serializer.save()
+                return JsonResponse(detail_serializer.data)
+            return JsonResponse(detail_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        elif request.method == 'DELETE':
+            mentor.delete()
+            return JsonResponse({'message': 'mentor was deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
+    except Mentor.DoesNotExist:
+        return JsonResponse({'message': 'The mentor does not exist'}, status=status.HTTP_404_NOT_FOUND)
+
+def student_list(request, pk):
+    pass
