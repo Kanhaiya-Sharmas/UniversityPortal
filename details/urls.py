142a81c2f2e2b32002865c4f86e6c370377a7ab5
Kanhaiya-Sharma <kanhaiya52cse@gmail.com>
2020-09-26 14:37:51 +0530

    Comments added

--- details/urls.py
+++ details/urls.py
@@ -2,14 +2,17 @@ from django.conf.urls import url
 from details import views
 
 urlpatterns = [
+    # Superuser can "list, add, delete" all the Mentors and Students,
     url(r'^api/superuser$', views.mentor_full),
     url(r'^api/superuser/student', views.student_full),
     url(r'^api/superuser/(?P<pk>[0-9]+)$', views.mentor_byid),
     url(r'^api/superuser/student', views.student_byid),
 
+    # Mentors can "list, add, delete" all the Students
     url(r'^api/mentor$', views.student_only),
     url(r'^api/mentor/(?P<pk>[0-9]+)$', views.student_onlybyid),
 
-    url(r'^api/mentor$', views.student_list),
-    url(r'^api/mentor/(?P<pk>[0-9]+)$', views.student_listbyid),
+    # Students can only "list" all the Students details and details by id
+    url(r'^api/student$', views.student_list),
+    url(r'^api/student/(?P<pk>[0-9]+)$', views.student_listbyid),
 ]
