b3b65be9781b7595431f1e88e0095d6ffee77f69
Kanhaiya-Sharma <kanhaiya52cse@gmail.com>
2020-09-26 08:45:47 +0530

    adding CORS middleware

--- College/settings.py
+++ College/settings.py
@@ -51,6 +51,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # CORS
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'College.urls'
