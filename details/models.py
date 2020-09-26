56942739d56bda5b365f0507a7193284b91d37e4
Kanhaiya-Sharma <kanhaiya52cse@gmail.com>
2020-09-26 11:16:33 +0530

    changes in table +ading student table

--- details/models.py
+++ details/models.py
@@ -2,8 +2,17 @@ from django.db import models
 
 
 # Create your models here.
-class Details(models.Model):
-    Mentor_Name = models.CharField(max_length=50,blank=False, default='')
-    Student_Name = models.CharField(max_length=50, blank=False, default='')
-    Status = models.CharField(max_length=50, blank=False, default='')
+class Mentor(models.Model):
+    mentor = models.CharField(max_length=50, blank=False, default='')
+    lecture = models.CharField(max_length=50, blank=False, default='')
+    status = models.BooleanField(default=False)
+    Remark = models.CharField(max_length=50, blank=False, default='')
+
+
+class Student(models.Model):
+    student = models.CharField(max_length=50, blank=False, default='')
+    math = models.CharField(max_length=50, blank=False, default='')
+    physics = models.CharField(max_length=50, blank=False, default='')
+    english = models.CharField(max_length=50, blank=False, default='')
+    status = models.BooleanField(default=False)
     Remark = models.CharField(max_length=50, blank=False, default='')
