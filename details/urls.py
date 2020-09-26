8828d1a437a59f7088976297398b244c051856a1
Kanhaiya-Sharma <kanhaiya52cse@gmail.com>
2020-09-26 13:49:50 +0530

    adding urls, and view rights

--- details/urls.py
+++ details/urls.py
@@ -2,9 +2,14 @@ from django.conf.urls import url
 from details import views
 
 urlpatterns = [
-    url(r'^api/mentor$', views.mentor_list),
+    url(r'^api/superuser$', views.mentor_full),
+    url(r'^api/superuser$', views.student_full),
+    url(r'^api/superuser/(?P<pk>[0-9]+)$', views.mentor_byid),
+
+    url(r'^api/mentor$', views.list_menortor),
+    url(r'^api/mentor$$', views.student_list),
     url(r'^api/mentor/(?P<pk>[0-9]+)$', views.mentor_detail),
-    url(r'^api/student$', views.student_list),
+
 
 
 ]
--- details/views.py
+++ details/views.py
@@ -5,14 +5,17 @@ from rest_framework.parsers import JSONParser
 from rest_framework import status
 
 from details.models import Mentor
+from details.models import Student
 from details.serializers import MentorSerializer
+from details.serializers import StudentSerializer
 from rest_framework.decorators import api_view
 
 
-# Create your views here.
+# Create your views here
+
 # START function mentor_list
 @api_view(['GET', 'POST', 'DELETE'])
-def mentor_list(request):
+def mentor_full(request):
     # Get all mentor list
     if request.method == 'GET':
         details = Mentor.objects.all()
@@ -45,7 +48,7 @@ def mentor_list(request):
 
 # START function mentor_details by id
 @api_view(['GET', 'PUT', 'DELETE'])
-def mentor_detail(request, pk):
+def mentor_byid(request, pk):
     try:
         mentor = Mentor.objects.get(pk=pk)
 
@@ -65,5 +68,55 @@ def mentor_detail(request, pk):
     except Mentor.DoesNotExist:
         return JsonResponse({'message': 'The mentor does not exist'}, status=status.HTTP_404_NOT_FOUND)
 
-def student_list(request, pk):
-    pass
+
+@api_view(['GET', 'POST', 'DELETE'])
+def student_full(request):
+    # Get all student list
+    if request.method == 'GET':
+        details = Student.objects.all()
+
+        student = request.GET.get('student', None)
+        if student is not None:
+            details = details.filter(student__icontains=student)
+
+        details_serializer = StudentSerializer(details, many=True)
+        return JsonResponse(details_serializer.data, safe=False)
+    # Add a new student
+    elif request.method == 'POST':
+        student_data = JSONParser().parse(request)
+        student_serializer = StudentSerializer(data=student_data)
+        if student_serializer.is_valid():
+            student_serializer.save()
+            return JsonResponse(student_serializer.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(student_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # Delete all student
+    elif request.method == 'DELETE':
+        count = Student.objects.all().delete()
+        return JsonResponse({'message': '{} details were deleted successfully!'.format(count[0])},
+                            status=status.HTTP_204_NO_CONTENT)
+    elif request.method == 'DELETE':
+        count = Student.objects.all().delete()
+        return JsonResponse({'message': '{} details were deleted successfully!'.format(count[0])},
+                            status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def mentor_byid(request, pk):
+    try:
+        student = Student.objects.get(pk=pk)
+
+        if request.method == 'GET':
+            detail_serializer = StudentSerializer(student)
+            return JsonResponse(detail_serializer.data)
+        elif request.method == 'PUT':
+            detail_data = JSONParser().parse(request)
+            detail_serializer = StudentSerializer(student, data=detail_data)
+            if detail_serializer.is_valid():
+                detail_serializer.save()
+                return JsonResponse(detail_serializer.data)
+            return JsonResponse(detail_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        elif request.method == 'DELETE':
+            student.delete()
+            return JsonResponse({'message': 'student was deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
+    except Student.DoesNotExist:
+        return JsonResponse({'message': 'The student does not exist'}, status=status.HTTP_404_NOT_FOUND)
